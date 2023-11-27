bb7b7c52ffbe3081527a3e2b7afe7d3c6843ed16
VIKAS-1007 <vikasrana7867@gmail.com>
2023-11-27 18:04:58 +0530

    first commit for image gallery

---
+++ app.js
@@ -0,0 +1,57 @@
+let content = document.getElementsByTagName("figure");
+console.log(content);
+
+content[0].addEventListener("dblclick", function (e) {
+  window.location.href = "https://en.wikipedia.org/wiki/British_Museum";
+});
+
+content[1].addEventListener("dblclick", function (e) {
+  window.location.href =
+    "https://en.wikipedia.org/wiki/Natural_History_Museum,_London";
+});
+
+content[2].addEventListener("dblclick", function (e) {
+  window.location.href =
+    "https://images.pexels.com/photos/3148596/pexels-photo-3148596.jpeg?auto=compress&cs=tinysrgb&w=600d";
+});
+
+content[3].addEventListener("dblclick", function (e) {
+  window.location.href = "https://www.caferouge.com/";
+});
+content[4].addEventListener("dblclick", function (e) {
+  window.location.href = "https://en.wikipedia.org/wiki/Galfa_Tower";
+});
+content[5].addEventListener("dblclick", function (e) {
+  window.location.href = "https://en.wikipedia.org/wiki/Granada_Cathedral";
+});
+content[6].addEventListener("dblclick", function (e) {
+  window.location.href =
+    "https://medium.com/thebeammagazine/museums-and-climate-change-activism-cc0561e50731";
+});
+content[7].addEventListener("dblclick", function (e) {
+  window.location.href = "https://en.wikipedia.org/wiki/Tribune_Tower";
+});
+content[8].addEventListener("dblclick", function (e) {
+  window.location.href =
+    "https://www.pexels.com/photo/low-angle-photo-of-white-concrete-building-under-blue-sky-1233262/";
+});
+content[9].addEventListener("dblclick", function (e) {
+  window.location.href = "https://en.wikipedia.org/wiki/Zehlendorf_(Berlin)";
+});
+content[10].addEventListener("dblclick", function (e) {
+  window.location.href = "https://case.edu/";
+});
+content[11].addEventListener("dblclick", function (e) {
+  window.location.href = "https://www.tokyo-skytree.jp/en/";
+});
+content[12].addEventListener("dblclick", function (e) {
+  window.location.href = "https://en.wikipedia.org/wiki/KOIN_Tower";
+});
+content[13].addEventListener("dblclick", function (e) {
+  window.location.href =
+    "https://pixabay.com/photos/building-architecture-office-5808564/";
+});
+content[14].addEventListener("dblclick", function (e) {
+  window.location.href =
+    "https://www.pexels.com/photo/contemporary-building-corner-on-sunny-day-5087167/";
+});
